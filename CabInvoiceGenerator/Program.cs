222570368d80e0415b2028b2c2a32c40ac4346ee
Suraj Garud <surajgarud95@gmail.com>
2022-02-17 14:49:29 +0530

    Cab Invoice All UC

---
+++ CabInvoiceGenerator/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CabInvoiceGenerator;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Welcome to Cab Invoice Generator");
+InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+double fare = invoiceGenerator.CalculateFare(2.0, 5);
+Console.WriteLine($"Fare : {fare}");
+Console.ReadLine();
